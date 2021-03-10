3e9c0a19199d46422a20cc579f02eff9deaac677
saket1111 <saketrj2@gmail.com>
2021-03-10 22:33:28 +0530

    reload when clearing list

--- script/clearlist.js
+++ script/clearlist.js
@@ -2,4 +2,5 @@ export const clearList = () => {
     (document.getElementById('list').innerHTML = '');
     let container = document.querySelector('ul');
     container.parentNode.style.display = "none";
+    location.reload();
 }
